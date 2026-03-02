d1c26ed7147932d65ad67edfc75c80c2ad994b6b
pedro prado <pedro.prado@ptractor.com.br>
2026-03-02 06:53:34 -0300

    feat: add prisma

--- back-end/src/env/index.ts
+++ back-end/src/env/index.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import z from 'zod';
 
 const envSchema = z.object({
-  //   DATABASE_URL: z.string(),
+  DATABASE_URL: z.string(),
   HOST: z.string().default('127.0.0.1'),
   PORT: z.string().default('3333'),
   GROQ_API_KEY: z.string(),
